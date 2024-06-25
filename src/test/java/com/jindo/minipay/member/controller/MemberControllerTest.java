0662a1ea3d959c2c5578a164f6de94adf257a96d
cchoijjinyoung <cgy7127@gmail.com>
2024-06-25 17:54:46 +0900

    test: 회원 등록 & 메인 계좌 생성 API

---
+++ src/test/java/com/jindo/minipay/member/controller/MemberControllerTest.java
@@ -0,0 +1,87 @@
+package com.jindo.minipay.member.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jindo.minipay.member.dto.MemberSignupDto;
+import com.jindo.minipay.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class MemberControllerTest {
+
+  @MockBean
+  MemberService memberService;
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  static final String URI = "/member";
+
+  @Nested
+  @DisplayName("회원 등록")
+  class MemberSignupMethod {
+
+    @Test
+    @DisplayName("실패 - 유저네임이 빈 값인 경우")
+    void signup_empty_username() throws Exception {
+      // given
+      MemberSignupDto memberSignupDto = MemberSignupDto.builder()
+          .username("")
+          .password("1q2w3e4r!")
+          .build();
+
+      // when
+      // then
+      mockMvc.perform(post(URI)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(memberSignupDto)))
+          .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("실패 - 패스워드가 빈 값인 경우")
+    void signup_empty_password() throws Exception {
+      // given
+      MemberSignupDto memberSignupDto = MemberSignupDto.builder()
+          .username("username1")
+          .password("")
+          .build();
+
+      // when
+      // then
+      mockMvc.perform(post(URI)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(memberSignupDto)))
+          .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("성공")
+    void signup() throws Exception {
+      // given
+      MemberSignupDto memberSignupDto = MemberSignupDto.builder()
+          .username("username1")
+          .password("1q2w3e4r!")
+          .build();
+
+      // when
+      // then
+      mockMvc.perform(post(URI)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(memberSignupDto)))
+          .andExpect(status().isCreated());
+    }
+  }
+}
---
+++ src/test/java/com/jindo/minipay/member/service/MemberServiceTest.java
@@ -0,0 +1,88 @@
+package com.jindo.minipay.member.service;
+
+import static com.jindo.minipay.account.constant.AccountConstants.MAIN_ACCOUNT_PREFIX;
+import static com.jindo.minipay.account.entity.type.AccountType.MAIN;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.jindo.minipay.account.repository.AccountRepository;
+import com.jindo.minipay.account.util.AccountNumberCreator;
+import com.jindo.minipay.global.exception.CustomException;
+import com.jindo.minipay.global.exception.ErrorCode;
+import com.jindo.minipay.member.dto.MemberSignupDto;
+import com.jindo.minipay.member.entity.Member;
+import com.jindo.minipay.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+  @Mock
+  MemberRepository memberRepository;
+
+  @Mock
+  AccountRepository accountRepository;
+
+  @Mock
+  AccountNumberCreator accountNumberCreator;
+
+  @InjectMocks
+  MemberService memberService;
+
+  @Nested
+  @DisplayName("회원 등록 메서드")
+  class MemberSignupMethod {
+
+    MemberSignupDto memberSignupDto = MemberSignupDto.builder()
+        .username("username1")
+        .password("1q2w3e4r!")
+        .build();
+
+    @Test
+    @DisplayName("실패 - 유저네임이 중복된 경우")
+    void signup_conflict_username() {
+      // given
+
+      // when
+      given(memberRepository.existsByUsername(memberSignupDto.getUsername())).willReturn(true);
+
+      // then
+      assertThatThrownBy(() -> memberService.signup(memberSignupDto))
+          .isInstanceOf(CustomException.class)
+          .hasMessage(ErrorCode.ALREADY_EXISTS_USERNAME.getMessage());
+    }
+
+    @Test
+    @DisplayName("성공 - 회원과 메인계좌가 생성된다.")
+    void signup() {
+      // given
+      given(memberRepository.existsByUsername(memberSignupDto.getUsername())).willReturn(false);
+
+      Member owner = Member.builder()
+          .id(1L)
+          .username(memberSignupDto.getUsername())
+          .password(memberSignupDto.getPassword())
+          .build();
+
+      // when
+      when(memberRepository.save(any())).thenReturn(owner);
+
+      String accountNumber = MAIN_ACCOUNT_PREFIX + "12345678";
+      when(accountNumberCreator.create(MAIN)).thenReturn(accountNumber);
+
+      memberService.signup(memberSignupDto);
+
+      // then
+      verify(accountRepository).save(any());
+    }
+  }
+}
