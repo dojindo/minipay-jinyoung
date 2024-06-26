9cc009d620252227e7c9604e23245e92b69f10ed
cchoijjinyoung <cgy7127@gmail.com>
2024-06-26 20:03:48 +0900

    refactor: Account 엔티티 구조 변경으로 인한 회원서비스 로직 수정

--- src/main/java/com/jindo/minipay/member/service/MemberService.java
+++ src/main/java/com/jindo/minipay/member/service/MemberService.java
@@ -1,11 +1,11 @@
 package com.jindo.minipay.member.service;
 
+import static com.jindo.minipay.account.common.type.AccountType.CHECKING;
 import static com.jindo.minipay.global.exception.ErrorCode.ALREADY_EXISTS_USERNAME;
 
-import com.jindo.minipay.account.entity.Account;
-import com.jindo.minipay.account.entity.type.AccountType;
-import com.jindo.minipay.account.repository.AccountRepository;
-import com.jindo.minipay.account.util.AccountNumberCreator;
+import com.jindo.minipay.account.checking.entity.CheckingAccount;
+import com.jindo.minipay.account.checking.repository.CheckingAccountRepository;
+import com.jindo.minipay.account.common.util.AccountNumberCreator;
 import com.jindo.minipay.global.exception.CustomException;
 import com.jindo.minipay.member.dto.MemberSignupDto;
 import com.jindo.minipay.member.entity.Member;
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
   private final MemberRepository memberRepository;
-  private final AccountRepository accountRepository;
+  private final CheckingAccountRepository checkingAccountRepository;
   private final AccountNumberCreator accountNumberCreator;
 
   @Transactional
@@ -35,12 +35,12 @@ public class MemberService {
 
     Member owner = memberRepository.save(member);
 
-    Account account = Account.builder()
+    // TODO : 계좌 생성 분리하기
+    CheckingAccount checkingAccount = CheckingAccount.builder()
         .owner(owner)
-        .accountNumber(accountNumberCreator.create(AccountType.MAIN))
-        .type(AccountType.MAIN)
+        .accountNumber(accountNumberCreator.create(CHECKING))
         .build();
 
-    accountRepository.save(account);
+    checkingAccountRepository.save(checkingAccount);
   }
 }
