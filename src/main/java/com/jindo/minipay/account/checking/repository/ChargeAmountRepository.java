155088cbf85d46e7d50e317ce4eb40978debed16
cchoijjinyoung <cgy7127@gmail.com>
2024-06-27 11:54:57 +0900

    feat: 메인 계좌 충전 API
    - ttl을 동적으로 설정해주기 위해, RestTemplate를 사용했습니다.
    - Redis에 저장될 객체인 ChargeAmount를 insert, select해올 수 있는 ChargeAmountRepository 에서 RestTemplate를 사용하도록 설계하였습니다.

---
+++ src/main/java/com/jindo/minipay/account/checking/repository/ChargeAmountRepository.java
@@ -0,0 +1,26 @@
+package com.jindo.minipay.account.checking.repository;
+
+import com.jindo.minipay.account.checking.entity.ChargeAmount;
+import com.jindo.minipay.account.common.constant.AccountConstants;
+import java.time.Duration;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ChargeAmountRepository {
+
+  private final RedisTemplate<String, Object> redisTemplate;
+
+  public void save(Long memberId, long amount, Duration ttl) {
+    ChargeAmount chargeAmount = new ChargeAmount(memberId, amount);
+    String key = AccountConstants.CHARGE_AMOUNT_KEY + ":" + memberId;
+    redisTemplate.opsForValue().set(key, chargeAmount, ttl);
+  }
+
+  public Optional<ChargeAmount> findByMemberId(Long memberId) {
+    return Optional.ofNullable((ChargeAmount) redisTemplate.opsForValue().get(AccountConstants.CHARGE_AMOUNT_KEY + ":" + memberId));
+  }
+}
---
+++ src/main/java/com/jindo/minipay/account/common/constant/AccountConstants.java
@@ -0,0 +1,13 @@
+package com.jindo.minipay.account.common.constant;
+
+public class AccountConstants {
+
+  public static final String CHECKING_ACCOUNT_PREFIX = "1111";
+
+  public static final String SAVING_ACCOUNT_PREFIX = "1122";
+
+  public static final int ACCOUNT_CHARGE_LIMIT = 3_000_000;
+
+  public static final String CHARGE_AMOUNT_KEY = "chargeAmount:";
+
+}
