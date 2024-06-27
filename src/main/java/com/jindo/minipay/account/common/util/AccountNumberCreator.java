41fa8c0798f7d4d6eba6492f645e0c0496b238b6
cchoijjinyoung <cgy7127@gmail.com>
2024-06-27 22:06:38 +0900

    refactor: AccountNumberCreator 수정
    - AccountNumberCreator를 인터페이스로 두고, 팩토리패턴을 사용하는 방법과, AccountRepository를 공통 인터페이스로 사용하는 방법을 고려하였습니다. 두 방법 모두 if/else-if를 사용하는 것보다 코드의 간결성이나 효율적인 측면에서 뚜렷한 장점이 있다고는 생각이 들지 않았기도하고, 추후에 리팩토링해도 되는 문제라고 생각해서 if/else-if를 그대로 사용하였습니다.

--- src/main/java/com/jindo/minipay/account/common/util/AccountNumberCreator.java
+++ src/main/java/com/jindo/minipay/account/common/util/AccountNumberCreator.java
@@ -7,6 +7,7 @@ import static com.jindo.minipay.account.common.type.AccountType.SAVING;
 
 import com.jindo.minipay.account.checking.repository.CheckingAccountRepository;
 import com.jindo.minipay.account.common.type.AccountType;
+import com.jindo.minipay.account.savings.repository.SavingAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class AccountNumberCreator {
 
   private final CheckingAccountRepository checkingAccountRepository;
+  private final SavingAccountRepository savingAccountRepository;
 
   public String create(AccountType accountType) {
     StringBuilder sb = new StringBuilder();
@@ -29,7 +31,12 @@ public class AccountNumberCreator {
       sb.append((int) (Math.random() * 10));
     }
 
-    boolean exists = checkingAccountRepository.existsByAccountNumber(sb.toString());
+    boolean exists = false;
+    if (accountType == CHECKING) {
+      exists = checkingAccountRepository.existsByAccountNumber(sb.toString());
+    } else if (accountType == SAVING) {
+      exists = savingAccountRepository.existsByAccountNumber(sb.toString());
+    }
 
     if (exists) {
       return create(accountType);
