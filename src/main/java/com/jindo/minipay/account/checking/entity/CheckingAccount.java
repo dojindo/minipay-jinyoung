6a71450a0e3b7a38ca5fb58c01a257e407392445
cchoijjinyoung <cgy7127@gmail.com>
2024-06-27 22:03:04 +0900

    refactor: CheckingAccount FetchType.LAZY 추가

--- src/main/java/com/jindo/minipay/account/checking/entity/CheckingAccount.java
+++ src/main/java/com/jindo/minipay/account/checking/entity/CheckingAccount.java
@@ -4,6 +4,7 @@ import com.jindo.minipay.global.entity.BaseTimeEntity;
 import com.jindo.minipay.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,7 +31,7 @@ public class CheckingAccount extends BaseTimeEntity {
 
   private long balance;
 
-  @OneToOne
+  @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(nullable = false)
   private Member owner;
 
