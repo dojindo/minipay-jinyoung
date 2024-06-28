a02cf352d5830c775a00516197597756545f0f00
cchoijjinyoung <cgy7127@gmail.com>
2024-06-28 19:08:02 +0900

    feat: 메인 계좌와 적금 계좌간 동시성 이슈 해결
    - 한 쪽 트랜젝션이 select를 했으면 다른 쪽 트랜잭션은 select를 해선 안돼서 비관적락을 사용했습니다.
    - 현재 상황은 다른 트랜잭션이 read하는 것을 허용하면 안되기 때문에 '쓰기 락'으로 선택했습니다.
    - 비관적 락이 이미 동시성 문제를 차단하기 때문에 트랜잭션 격리 레벨은 뚜렷한 효과가 있다고 생각이 들진 않았습니다. 하지만 SERIALIZABLE로 했을 경우에는, 새로운 회원이 가입했을 때, 그 회원의 메인 계좌 insert도 막아버리기 때문에, 트랜잭션 격리레벨도 최소한으로 설정해주는 것이 좋다고 판단하였고, 두 서비스 메서드 모두 READ_COMMITTED로 설정해주었습니다.

--- src/main/java/com/jindo/minipay/account/checking/repository/CheckingAccountRepository.java
+++ src/main/java/com/jindo/minipay/account/checking/repository/CheckingAccountRepository.java
@@ -1,12 +1,21 @@
 package com.jindo.minipay.account.checking.repository;
 
+import static jakarta.persistence.LockModeType.PESSIMISTIC_WRITE;
+
 import com.jindo.minipay.account.checking.entity.CheckingAccount;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CheckingAccountRepository extends JpaRepository<CheckingAccount, Long> {
 
   boolean existsByAccountNumber(String accountNumber);
 
   Optional<CheckingAccount> findByOwnerId(Long ownerId);
+
+  @Lock(PESSIMISTIC_WRITE)
+  @Query("SELECT c FROM CheckingAccount c WHERE c.owner.id = :owner_id")
+  Optional<CheckingAccount> findByOwnerIdForUpdate(@Param("owner_id") Long ownerId);
 }
