af1f77bf92ce3b15b3083a74491f6fdaaf6ef3ad
cchoijjinyoung <cgy7127@gmail.com>
2024-06-28 19:00:53 +0900

    refactor: 적금 계좌 개설 Response dto 생성

---
+++ src/main/java/com/jindo/minipay/account/savings/dto/SavingAccountCreateResponse.java
@@ -0,0 +1,14 @@
+package com.jindo.minipay.account.savings.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavingAccountCreateResponse {
+
+  private Long id;
+
+}
