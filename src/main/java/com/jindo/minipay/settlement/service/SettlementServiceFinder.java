7d4d885be1a603a0ad5b028958ba7aabb01ae0de
cchoijjinyoung <cgy7127@gmail.com>
2024-07-18 20:56:13 +0900

    feat: SettlementServiceFinder 작성
    - SettleService를 상속받는 EqualSettlementService와 RandomSettlementService를 작성하였고, request로부터 넘어오는 SettlementType 별로 1/N 정산 혹은 랜덤 정산 서비스를 제공하고 싶었기 때문에, Finder 클래스로 각 서비스를 찾도록 작성하였습니다.

---
+++ src/main/java/com/jindo/minipay/settlement/service/SettlementServiceFinder.java
@@ -0,0 +1,21 @@
+package com.jindo.minipay.settlement.service;
+
+import static com.jindo.minipay.global.exception.ErrorCode.NOT_EXISTS_SETTLE_TYPE;
+
+import com.jindo.minipay.settlement.type.SettlementType;
+import com.jindo.minipay.global.exception.CustomException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SettlementServiceFinder {
+
+  private final List<SettlementService> settlementServices;
+
+  public SettlementService find(SettlementType settlementType) {
+    return settlementServices.stream().filter(settlementService -> settlementService.getType() == settlementType)
+        .findAny().orElseThrow(() -> new CustomException(NOT_EXISTS_SETTLE_TYPE));
+  }
+}
