55e01a3a3fabad188f1e65f1d79378814f551379
cchoijjinyoung <cgy7127@gmail.com>
2024-11-02 23:17:56 +0900

    feat: 회원가입 시 회원설정 자동 생성

--- src/main/java/com/jindo/minipay/member/service/MemberService.java
+++ src/main/java/com/jindo/minipay/member/service/MemberService.java
@@ -2,20 +2,18 @@ package com.jindo.minipay.member.service;
 
 import static com.jindo.minipay.global.exception.ErrorCode.ALREADY_EXISTS_USERNAME;
 
-import com.jindo.minipay.member.entity.MemberSettings;
-import com.jindo.minipay.member.event.MemberSignupEvent;
 import com.jindo.minipay.global.exception.CustomException;
 import com.jindo.minipay.member.dto.MemberSignupRequest;
 import com.jindo.minipay.member.entity.Member;
+import com.jindo.minipay.member.entity.MemberSettings;
+import com.jindo.minipay.member.event.MemberSignupEvent;
 import com.jindo.minipay.member.repository.MemberRepository;
 import com.jindo.minipay.member.repository.MemberSettingsRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -37,7 +35,7 @@ public class MemberService {
     eventPublisher.publishEvent(MemberSignupEvent.of(member.getId()));
 
     return member.getId();
-}
+  }
 
   private void saveMemberSettings(Member member) {
     MemberSettings settings = MemberSettings.builder()
