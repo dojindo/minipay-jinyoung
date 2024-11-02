15cbb51fd79e01e3fbace55297460ab5d8f7b9fc
cchoijjinyoung <cgy7127@gmail.com>
2024-11-02 22:34:40 +0900

    feat: 메인계좌서비스로부터 충전서비스와 송금서비스를 분리
    - 메인게좌서비스가 조금 더 작은 범위의 기능을 수행하도록 클래스를 분리하였습니다.

--- src/main/java/com/jindo/minipay/account/checking/service/CheckingAccountService.java
+++ src/main/java/com/jindo/minipay/account/checking/service/CheckingAccountService.java
@@ -1,26 +1,18 @@
 package com.jindo.minipay.account.checking.service;
 
-import static com.jindo.minipay.account.common.constant.AccountConstants.ACCOUNT_CHARGE_LIMIT;
-import static com.jindo.minipay.account.common.constant.AccountConstants.AUTO_CHARGE_UNIT;
 import static com.jindo.minipay.account.common.type.AccountType.CHECKING;
-import static com.jindo.minipay.global.exception.ErrorCode.ACCOUNT_NOT_FOUND;
-import static com.jindo.minipay.global.exception.ErrorCode.CHARGE_LIMIT_EXCEEDED;
 import static com.jindo.minipay.global.exception.ErrorCode.MEMBER_NOT_FOUND;
 
+import com.jindo.minipay.account.checking.dto.ChargeResponse;
 import com.jindo.minipay.account.checking.dto.CheckingAccountChargeRequest;
-import com.jindo.minipay.account.checking.dto.CheckingAccountChargeResponse;
-import com.jindo.minipay.account.checking.dto.CheckingAccountWireRequest;
-import com.jindo.minipay.account.checking.dto.CheckingAccountWireResponse;
-import com.jindo.minipay.account.checking.entity.ChargeAmount;
+import com.jindo.minipay.account.checking.dto.CheckingAccountRemitRequest;
+import com.jindo.minipay.account.checking.dto.RemitResponse;
 import com.jindo.minipay.account.checking.entity.CheckingAccount;
 import com.jindo.minipay.account.checking.repository.CheckingAccountRepository;
-import com.jindo.minipay.account.checking.repository.redis.ChargeAmountRepository;
 import com.jindo.minipay.account.common.util.AccountNumberCreator;
 import com.jindo.minipay.global.exception.CustomException;
 import com.jindo.minipay.member.entity.Member;
 import com.jindo.minipay.member.repository.MemberRepository;
-import java.time.Duration;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -34,7 +26,8 @@ public class CheckingAccountService {
   private final CheckingAccountRepository checkingAccountRepository;
   private final MemberRepository memberRepository;
   private final AccountNumberCreator accountNumberCreator;
-  private final ChargeAmountRepository chargeAmountRepository;
+  private final ChargeService chargeService;
+  private final RemitServiceFinder remitServiceFinder;
 
   public void create(Long memberId) {
     Member owner = memberRepository.findById(memberId)
@@ -49,88 +42,12 @@ public class CheckingAccountService {
   }
 
   @Transactional
-  public CheckingAccountChargeResponse charge(CheckingAccountChargeRequest request) {
-    CheckingAccount checkingAccount = getCheckingAccountForUpdate(request.getMemberId());
-
-    actualCharge(checkingAccount, request.getAmount());
-
-    return new CheckingAccountChargeResponse(checkingAccount.getBalance());
+  public ChargeResponse charge(CheckingAccountChargeRequest request) {
+    return new ChargeResponse(chargeService.charge(request.getMemberId(), request.getAmount()));
   }
 
   @Transactional
-  public CheckingAccountWireResponse wire(CheckingAccountWireRequest request) {
-
-    // TODO: 데드락 해결하기
-    Long senderId = request.getSenderId();
-    Long receiverId = request.getReceiverId();
-
-    CheckingAccount senderAccount;
-    CheckingAccount receiverAccount;
-
-    if (senderId < receiverId) {
-      senderAccount = getCheckingAccountForUpdate(senderId);
-      receiverAccount = getCheckingAccountForUpdate(receiverId);
-    } else {
-      receiverAccount = getCheckingAccountForUpdate(receiverId);
-      senderAccount = getCheckingAccountForUpdate(senderId);
-    }
-
-    checkBalanceAndAutoCharge(senderAccount, request.getAmount());
-
-    senderAccount.withdraw(request.getAmount());
-
-    receiverAccount.deposit(request.getAmount());
-
-    return new CheckingAccountWireResponse(senderAccount.getBalance());
-  }
-
-  private CheckingAccount getCheckingAccountForUpdate(Long memberId) {
-    return checkingAccountRepository
-        .findByOwnerIdForUpdate(memberId)
-        .orElseThrow(() -> new CustomException(ACCOUNT_NOT_FOUND));
-  }
-
-  private void actualCharge(CheckingAccount checkingAccount, long amount) {
-    updateChargeAmountOfMember(checkingAccount.getOwner().getId(), amount);
-    checkingAccount.deposit(amount);
-  }
-
-  private void updateChargeAmountOfMember(Long memberId, long amount) {
-    long chargeAmount = getChargeAmountOfMember(memberId).getAmount();
-
-    long afterChargeAmount = chargeAmount + amount;
-    validateChargeLimit(afterChargeAmount);
-
-    chargeAmountRepository.save(memberId, afterChargeAmount, timeToMidnight());
-  }
-
-  private ChargeAmount getChargeAmountOfMember(Long memberId) {
-    return chargeAmountRepository.findByMemberId(memberId)
-        .orElseGet(() -> new ChargeAmount(memberId, 0L));
-  }
-
-  private void validateChargeLimit(long afterChargeAmount) {
-    if (afterChargeAmount > ACCOUNT_CHARGE_LIMIT) {
-      throw new CustomException(CHARGE_LIMIT_EXCEEDED);
-    }
-  }
-
-  private void checkBalanceAndAutoCharge(CheckingAccount checkingAccount, long amount) {
-    long balance = checkingAccount.getBalance();
-    if (balance < amount) {
-      long diff = amount - balance;
-      long chargeAmount = (diff / AUTO_CHARGE_UNIT) * AUTO_CHARGE_UNIT;
-
-      if (diff % ACCOUNT_CHARGE_LIMIT > 0) {
-        chargeAmount += AUTO_CHARGE_UNIT;
-      }
-      actualCharge(checkingAccount, chargeAmount);
-    }
-  }
-
-  private Duration timeToMidnight() {
-    LocalDateTime now = LocalDateTime.now();
-    LocalDateTime midnight = now.toLocalDate().atStartOfDay().plusDays(1);
-    return Duration.between(now, midnight);
+  public RemitResponse remit(CheckingAccountRemitRequest request) {
+    return remitServiceFinder.find(request.getSenderId()).remit(request);
   }
 }
--- src/test/java/com/jindo/minipay/account/checking/service/CheckingAccountServiceTest.java
+++ src/test/java/com/jindo/minipay/account/checking/service/CheckingAccountServiceTest.java
@@ -1,32 +1,25 @@
 package com.jindo.minipay.account.checking.service;
 
-import static com.jindo.minipay.account.common.constant.AccountConstants.ACCOUNT_CHARGE_LIMIT;
-import static com.jindo.minipay.global.exception.ErrorCode.ACCOUNT_NOT_FOUND;
-import static com.jindo.minipay.global.exception.ErrorCode.CHARGE_LIMIT_EXCEEDED;
 import static com.jindo.minipay.global.exception.ErrorCode.MEMBER_NOT_FOUND;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
+import com.jindo.minipay.account.checking.dto.ChargeResponse;
 import com.jindo.minipay.account.checking.dto.CheckingAccountChargeRequest;
-import com.jindo.minipay.account.checking.dto.CheckingAccountChargeResponse;
-import com.jindo.minipay.account.checking.dto.CheckingAccountWireRequest;
-import com.jindo.minipay.account.checking.dto.CheckingAccountWireResponse;
-import com.jindo.minipay.account.checking.entity.ChargeAmount;
+import com.jindo.minipay.account.checking.dto.CheckingAccountRemitRequest;
+import com.jindo.minipay.account.checking.dto.RemitResponse;
 import com.jindo.minipay.account.checking.entity.CheckingAccount;
 import com.jindo.minipay.account.checking.repository.CheckingAccountRepository;
-import com.jindo.minipay.account.checking.repository.redis.ChargeAmountRepository;
 import com.jindo.minipay.account.common.util.AccountNumberCreator;
 import com.jindo.minipay.global.exception.CustomException;
 import com.jindo.minipay.member.entity.Member;
 import com.jindo.minipay.member.repository.MemberRepository;
-import java.time.Duration;
 import java.util.Optional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -45,21 +38,26 @@ class CheckingAccountServiceTest {
   CheckingAccountRepository checkingAccountRepository;
 
   @Mock
-  ChargeAmountRepository chargeAmountRepository;
-
-  @Mock
   MemberRepository memberRepository;
 
   @Mock
   AccountNumberCreator accountNumberCreator;
 
+  @Mock
+  ChargeService chargeService;
+
+  @Mock
+  RemitServiceFinder remitServiceFinder;
+
+
   @Nested
   @DisplayName("메인 계좌 생성")
   class CheckingAccountCreateMethod {
+
     Long memberId = 1L;
 
     @Test
-    @DisplayName("싪패 - 존재하지 않는 회원알 때")
+    @DisplayName("메인 계좌 생성 실패 - 존재하지 않는 회원알 때")
     void create_member_not_found() {
       // given
       given(memberRepository.findById(memberId)).willThrow(new CustomException(MEMBER_NOT_FOUND));
@@ -72,7 +70,7 @@ class CheckingAccountServiceTest {
     }
 
     @Test
-    @DisplayName("성공")
+    @DisplayName("메인 계좌 생성 성공")
     void create_success() {
       // given
       Member member = Member.builder()
@@ -97,224 +95,72 @@ class CheckingAccountServiceTest {
 
     CheckingAccountChargeRequest request = new CheckingAccountChargeRequest(1L, amount);
 
-    Member owner = Member.builder()
-        .id(request.getMemberId())
-        .build();
-
-    CheckingAccount checkingAccount = CheckingAccount.builder()
-        .id(1L)
-        .owner(owner)
-        .accountNumber("111112345678")
-        .balance(request.getAmount())
-        .build();
-
     @Test
-    @DisplayName("실패 - 없는 계좌일 때")
-    void charge_not_found_checking_account() {
+    @DisplayName("메인 계좌 충전 성공")
+    void charge_success() {
       // given
-      given(checkingAccountRepository.findByOwnerIdForUpdate(request.getMemberId()))
-          .willThrow(new CustomException(ACCOUNT_NOT_FOUND));
+      given(chargeService.charge(request.getMemberId(), amount))
+          .willReturn(10_000L);
 
       // when
-      // then
-      assertThatThrownBy(() -> checkingAccountService.charge(request))
-          .isInstanceOf(CustomException.class)
-          .hasMessage(ACCOUNT_NOT_FOUND.getMessage());
-    }
-
-    @Test
-    @DisplayName("실패 - 일일 충전 한도를 초과했을 때")
-    void charge_limit_exceeded() {
-      // given
-      ChargeAmount limitMaximumAmount = new ChargeAmount(owner.getId(), ACCOUNT_CHARGE_LIMIT);
-
-      when(checkingAccountRepository.findByOwnerIdForUpdate(request.getMemberId())).thenReturn(
-          Optional.of(checkingAccount));
-
-      when(chargeAmountRepository.findByMemberId(request.getMemberId())).thenReturn(
-          Optional.of(limitMaximumAmount));
-
-      // when
-      // then
-      assertThatThrownBy(() -> checkingAccountService.charge(request))
-          .isInstanceOf(CustomException.class)
-          .hasMessage(CHARGE_LIMIT_EXCEEDED.getMessage());
-    }
-
-    @Test
-    @DisplayName("성공")
-    void charge_already_exists_redis() {
-      // given
-      ChargeAmount chargeAmount = new ChargeAmount(owner.getId(), 10_000L);
-
-      when(checkingAccountRepository.findByOwnerIdForUpdate(request.getMemberId())).thenReturn(
-          Optional.of(checkingAccount));
-
-      when(chargeAmountRepository.findByMemberId(request.getMemberId())).thenReturn(
-          Optional.of(chargeAmount));
-
-      // when
-      CheckingAccountChargeResponse response = checkingAccountService.charge(request);
+      ChargeResponse response = checkingAccountService.charge(request);
 
       // then
-      verify(chargeAmountRepository, times(1)).save(any(Long.class), any(Long.class),
-          any(Duration.class));
-      assertThat(response.getBalance()).isEqualTo(20_000L);
+      assertThat(response.getBalance()).isEqualTo(10_000L);
     }
   }
 
   @Nested
   @DisplayName("계좌 송금")
-  class CheckingAccountWireMethod {
+  class CheckingAccountRemitMethod {
 
-    long amount = 15_000L;
+    @Mock
+    ImmediateRemitService immediateRemitService;
 
-    CheckingAccountWireRequest request = new CheckingAccountWireRequest(1L, 2L, amount);
+    @Mock
+    PendingRemitService pendingRemitService;
+
+    Member sender = Member.builder()
+        .id(1L)
+        .build();
+
+    long amount = 10_000L;
+
+    CheckingAccountRemitRequest request = new CheckingAccountRemitRequest(sender.getId(), 2L,
+        amount);
 
     @Test
-    @DisplayName("실패 - 송신자의 메인계좌가 없는 경우")
-    void wire_not_found_sender_checking_account() {
+    @DisplayName("즉시 송금")
+    void immediateRemit() {
       // given
-      given(checkingAccountRepository.findByOwnerIdForUpdate(request.getSenderId()))
-          .willThrow(new CustomException(ACCOUNT_NOT_FOUND));
+      given(remitServiceFinder.find(sender.getId())).willReturn(immediateRemitService);
+
+      RemitResponse expectedResponse = new RemitResponse(amount);
+      given(immediateRemitService.remit(request)).willReturn(
+          new RemitResponse(amount));
 
       // when
+      RemitResponse response = checkingAccountService.remit(request);
+
       // then
-      assertThatThrownBy(() -> checkingAccountService.wire(request))
-          .isInstanceOf(CustomException.class)
-          .hasMessage(ACCOUNT_NOT_FOUND.getMessage());
+      assertThat(response.getBalance()).isEqualTo(expectedResponse.getBalance());
     }
 
     @Test
-    @DisplayName("실패 - 수신자의 메인계좌가 없는 경우")
-    void wire_not_found_receiver_checking_account() {
+    @DisplayName("보류 송금")
+    void pendingRemit() {
       // given
-      Member sender = Member.builder()
-          .id(1L)
-          .build();
+      given(remitServiceFinder.find(sender.getId())).willReturn(pendingRemitService);
 
-      CheckingAccount senderAccount = CheckingAccount.builder()
-          .id(1L)
-          .owner(sender)
-          .accountNumber("111112345678")
-          .build();
-
-      given(checkingAccountRepository.findByOwnerIdForUpdate(request.getSenderId()))
-          .willReturn(Optional.of(senderAccount));
-
-      given(checkingAccountRepository.findByOwnerIdForUpdate(request.getReceiverId()))
-          .willThrow(new CustomException(ACCOUNT_NOT_FOUND));
+      RemitResponse expectedResponse = new RemitResponse(amount);
+      given(pendingRemitService.remit(request)).willReturn(
+          new RemitResponse(amount));
 
       // when
+      RemitResponse response = checkingAccountService.remit(request);
+
       // then
-      assertThatThrownBy(() -> checkingAccountService.wire(request))
-          .isInstanceOf(CustomException.class)
-          .hasMessage(ACCOUNT_NOT_FOUND.getMessage());
-    }
-
-    @Nested
-    @DisplayName("송신자의 잔액이 송금 금액보다 적은 경우 - 자동 충전")
-    class isBalanceLessThanWireAmount {
-
-      Member sender = Member.builder()
-          .id(1L)
-          .build();
-
-      CheckingAccount senderAccount = CheckingAccount.builder()
-          .id(1L)
-          .owner(sender)
-          .accountNumber("111112345678")
-          .balance(0L)
-          .build();
-
-      Member receiver = Member.builder()
-          .id(2L)
-          .build();
-
-      CheckingAccount receiverAccount = CheckingAccount.builder()
-          .id(2L)
-          .owner(receiver)
-          .accountNumber("111198765432")
-          .balance(0L)
-          .build();
-
-      @Test
-      @DisplayName("실패 - 송신자의 일일 한도가 초과한 경우")
-      void wire_limit_exceeded() {
-        // given
-        ChargeAmount limitMaximumAmount = new ChargeAmount(sender.getId(), ACCOUNT_CHARGE_LIMIT);
-
-        when(checkingAccountRepository.findByOwnerIdForUpdate(request.getSenderId())).thenReturn(
-            Optional.of(senderAccount));
-
-        when(checkingAccountRepository.findByOwnerIdForUpdate(request.getReceiverId())).thenReturn(
-            Optional.of(receiverAccount));
-
-        when(chargeAmountRepository.findByMemberId(request.getSenderId())).thenReturn(
-            Optional.of(limitMaximumAmount));
-        // when
-        // then
-        assertThatThrownBy(() -> checkingAccountService.wire(request))
-            .isInstanceOf(CustomException.class)
-            .hasMessage(CHARGE_LIMIT_EXCEEDED.getMessage());
-      }
-
-      @Test
-      @DisplayName("성공 - 송신자의 계좌에 자동 충전 단위로 충전된 후 수신자의 계좌에 송금한다.")
-      void wire_autoCharge() {
-        // given
-        ChargeAmount chargeAmount = new ChargeAmount(sender.getId(), 0L);
-
-        when(checkingAccountRepository.findByOwnerIdForUpdate(request.getSenderId())).thenReturn(
-            Optional.of(senderAccount));
-
-        when(checkingAccountRepository.findByOwnerIdForUpdate(request.getReceiverId())).thenReturn(
-            Optional.of(receiverAccount));
-
-        when(chargeAmountRepository.findByMemberId(request.getSenderId())).thenReturn(
-            Optional.of(chargeAmount));
-
-        // when
-        CheckingAccountWireResponse response = checkingAccountService.wire(request);
-
-        // then
-        verify(chargeAmountRepository, times(1)).save(any(Long.class), anyLong(),
-            any(Duration.class));
-
-        assertThat(receiverAccount.getBalance()).isEqualTo(15_000L);
-
-        assertThat(response.getBalance()).isEqualTo(5_000L);
-      }
-
-      @Test
-      @DisplayName("성공 - 송신자의 계좌에 자동 충전 단위로 충전된 후 수신자의 계좌에 송금한다.")
-      void wire_autoCharge_zero_mod() {
-        // given
-        long zeroModAmount = 20000L;
-        CheckingAccountWireRequest request = new CheckingAccountWireRequest(1L, 2L, zeroModAmount);
-
-        ChargeAmount chargeAmount = new ChargeAmount(sender.getId(), 0L);
-
-        when(checkingAccountRepository.findByOwnerIdForUpdate(request.getSenderId())).thenReturn(
-            Optional.of(senderAccount));
-
-        when(checkingAccountRepository.findByOwnerIdForUpdate(request.getReceiverId())).thenReturn(
-            Optional.of(receiverAccount));
-
-        when(chargeAmountRepository.findByMemberId(request.getSenderId())).thenReturn(
-            Optional.of(chargeAmount));
-
-        // when
-        CheckingAccountWireResponse response = checkingAccountService.wire(request);
-
-        // then
-        verify(chargeAmountRepository, times(1)).save(any(Long.class), anyLong(),
-            any(Duration.class));
-
-        assertThat(receiverAccount.getBalance()).isEqualTo(20000L);
-
-        assertThat(response.getBalance()).isEqualTo(10_000L);
-      }
+      assertThat(response.getBalance()).isEqualTo(expectedResponse.getBalance());
     }
   }
 }
