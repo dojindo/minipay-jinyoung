a95e17d8a885c6c3c7e15d1995e5e4fcc905cb47
cchoijjinyoung <cgy7127@gmail.com>
2024-11-04 22:31:42 +0900

    feat: 통합테스트에서 사용되는 공통 메서드 분리

--- src/test/java/com/jindo/minipay/integration/IntegrationTestSupport.java
+++ src/test/java/com/jindo/minipay/integration/IntegrationTestSupport.java
@@ -1,8 +1,13 @@
 package com.jindo.minipay.integration;
 
+import static com.jindo.minipay.account.common.type.AccountType.CHECKING;
+import static com.jindo.minipay.account.common.type.AccountType.SAVING;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.jindo.minipay.account.checking.entity.CheckingAccount;
 import com.jindo.minipay.account.checking.repository.CheckingAccountRepository;
+import com.jindo.minipay.account.common.util.AccountNumberCreator;
+import com.jindo.minipay.account.savings.entity.SavingAccount;
 import com.jindo.minipay.account.savings.repository.SavingAccountRepository;
 import com.jindo.minipay.member.entity.Member;
 import com.jindo.minipay.member.entity.MemberSettings;
@@ -12,7 +17,9 @@ import com.jindo.minipay.pending.repository.PendingTransferRepository;
 import com.jindo.minipay.settlement.repository.ParticipantSettlementRepository;
 import com.jindo.minipay.settlement.repository.SettlementRepository;
 import io.restassured.RestAssured;
+import java.util.Optional;
 import java.util.Set;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +55,9 @@ public abstract class IntegrationTestSupport {
 
   @Autowired
   protected RedisTemplate<String, Object> redisTemplate;
+
+  @Autowired
+  protected AccountNumberCreator accountNumberCreator;
 
   @LocalServerPort
   int port;
@@ -85,5 +95,37 @@ public abstract class IntegrationTestSupport {
 
     memberSettingsRepository.save(settings);
     return member;
+  }
+
+  protected CheckingAccount saveCheckingAccount(Member owner, long balance) {
+    return checkingAccountRepository.save(
+        CheckingAccount.builder()
+            .owner(owner)
+            .accountNumber(accountNumberCreator.create(CHECKING))
+            .balance(balance)
+            .build());
+  }
+
+  protected SavingAccount saveSavingAccount(Member owner) {
+    return savingAccountRepository.save(
+        SavingAccount.builder()
+            .owner(owner)
+            .accountNumber(accountNumberCreator.create(SAVING))
+            .amount(0L)
+            .build());
+  }
+
+  protected void enableImmediateTransferOfMember(Member sender) {
+    Optional<MemberSettings> senderSettings = memberSettingsRepository.findByMember(sender);
+    Assertions.assertThat(senderSettings).isPresent();
+    senderSettings.get().enableImmediateTransfer();
+    memberSettingsRepository.save(senderSettings.get());
+  }
+
+  protected void disableImmediateTransferOfMember(Member sender) {
+    Optional<MemberSettings> senderSettings = memberSettingsRepository.findByMember(sender);
+    Assertions.assertThat(senderSettings).isPresent();
+    senderSettings.get().disableImmediateTransfer();
+    memberSettingsRepository.save(senderSettings.get());
   }
 }
