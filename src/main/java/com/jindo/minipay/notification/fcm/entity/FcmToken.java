816efd44df0f28b2a2b02f3d7b9ba6f88da594a6
cchoijjinyoung <cgy7127@gmail.com>
2024-12-23 00:34:20 +0900

    refactor: FcmToken 엔티티 Member 필드 Lazy loading으로 수정

--- src/main/java/com/jindo/minipay/notification/fcm/entity/FcmToken.java
+++ src/main/java/com/jindo/minipay/notification/fcm/entity/FcmToken.java
@@ -6,6 +6,7 @@ import com.jindo.minipay.global.entity.BaseTimeEntity;
 import com.jindo.minipay.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -26,7 +27,7 @@ public class FcmToken extends BaseTimeEntity {
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(nullable = false)
   private Member member;
 
