7c3758476a4fadf92e07a76440588ae5143ea4cc
cchoijjinyoung <cgy7127@gmail.com>
2024-12-22 18:36:07 +0900

    feat: FcmToken 엔티티 추가
    - 서버에 FCM 토큰 저장
    - 회원별로 여러 디바이스가 있을 수 있기 때문에 회원:토큰(1:N) 관계로 설정

---
+++ src/main/java/com/jindo/minipay/notification/fcm/entity/FcmToken.java
@@ -0,0 +1,36 @@
+package com.jindo.minipay.notification.fcm.entity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import com.jindo.minipay.global.entity.BaseTimeEntity;
+import com.jindo.minipay.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FcmToken extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(nullable = false)
+  private Member member;
+
+  @Column(nullable = false, unique = true)
+  private String token;
+
+}
