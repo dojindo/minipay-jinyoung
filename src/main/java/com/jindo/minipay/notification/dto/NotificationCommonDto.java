9f38d61cfcff8acb16ffb5eef689d64f24fa5f71
cchoijjinyoung <cgy7127@gmail.com>
2024-12-29 02:12:08 +0900

    refactor: DTO 클래스 lombok 추가

--- src/main/java/com/jindo/minipay/notification/dto/NotificationCommonDto.java
+++ src/main/java/com/jindo/minipay/notification/dto/NotificationCommonDto.java
@@ -1,9 +1,15 @@
 package com.jindo.minipay.notification.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationCommonDto {
 
   @NotNull
--- src/main/java/com/jindo/minipay/notification/fcm/dto/FcmTokenRegisterRequest.java
+++ src/main/java/com/jindo/minipay/notification/fcm/dto/FcmTokenRegisterRequest.java
@@ -3,10 +3,17 @@ package com.jindo.minipay.notification.fcm.dto;
 import com.jindo.minipay.member.entity.Member;
 import com.jindo.minipay.notification.fcm.entity.FcmToken;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FcmTokenRegisterRequest {
+
   @NotNull
   private Long memberId;
 
