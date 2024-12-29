be2b526ff2a3015df07e8bda5bfd370e9109beaa
cchoijjinyoung <cgy7127@gmail.com>
2024-12-29 15:17:37 +0900

    chore: 깃 이그노어 추가 및 단순 수정

--- src/main/java/com/jindo/minipay/notification/FcmTestController.java
+++ src/main/java/com/jindo/minipay/notification/FcmTestController.java
@@ -18,13 +18,13 @@ public class FcmTestController {
 
   @GetMapping("/test")
   public String test() {
-    return "index.html";
+    return "fcm_test.html";
   }
 
   /**
    * FCM 메시지 전송 테스트
    */
-  @PostMapping("/test")
+  @PostMapping("/fcm/test")
   @ResponseBody
   public ResponseEntity<Void> sendMessage(@RequestBody NotificationCommonDto dto) {
     // TODO: Service 클래스로 옮기기, FCM 전용 DTO 클래스 작성, fromCommonDto(dto) 메서드 포함
